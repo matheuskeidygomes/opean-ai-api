d5728b5fdbb0ee98fbf13dcda09074a51f60dd65
Matheus Keidy <74063350+matheuskeidygomes@users.noreply.github.com>
2023-02-19 11:01:39 -0300

    att

--- src/routes.ts
+++ src/routes.ts
@@ -1,27 +1,17 @@
-import { Router } from 'express';
-import { OpenAIApi, Configuration } from 'openai';
+import { Router, Request, Response } from 'express';
+import { callTextModelAI } from './openAI';
 
 export const routes = Router();
 
-routes.get("/", (req, res) => {
+routes.get("/", (req: Request, res: Response) => {
     return res.json({ welcome: "Hello! Welcome to OpenAI API!" });
 });
 
-routes.post('/gpt', async (req, res) => {
+routes.post('/ai/text', async (req: Request, res: Response) => {
     const { userMessage } = req.body;
-    const configurationAI = new Configuration({ apiKey: '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' });   // API Key
-    const openAI = new OpenAIApi(configurationAI);        
     try {
-        const responseAI = await openAI.createCompletion({          // Create completion
-            model: "text-davinci-003",                              // Model to use completion
-            prompt: userMessage,                                    // User message
-            temperature: 1,                                         // 0.0 to 1.0 (0.0 = no variation, 1.0 = most variation) 
-            max_tokens: 100,                                        // Number of tokens to generate (tokens = words)
-            //n: 2,                                                 // Number of results to return
-        });
-        //console.log(responseAI.data);
-        const messageAI = responseAI.data.choices[0].text?.trim();  // Get the AI response in text format
-        return res.json({ robot: messageAI });
+        const responseMessageAI = await callTextModelAI(userMessage);
+        return res.json({ robot: responseMessageAI });
     } catch (error: any) {
         console.log({ error: error.message });
     }
